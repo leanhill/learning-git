795565d3604d1bec942d4587ccdb713fb546ecff
Andrzej Kaniewski <andrzej.kaniewski@gmail.com>
2020-06-18 23:27:42 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+lista_zakupów={
+    "piekarnia":["chleb", "pączek","bułki"],
+    "warzywniak":["marchew", "seler","rukola"]
+}
+
+for i, a in lista_zakupów.items():
+    for b in range (len(a)):
+        a[b] = a[b].capitalize()
+    i = i.capitalize()
+    print("Idę do %s"%i,"i kupuję tu następujące rzeczy:%s"%a)
+sum = sum([len(a) for i, a in lista_zakupów.items()])
+print("W sumie kupuję %s"%sum, "produktów")
